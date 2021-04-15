b6b70eeab6fe2e651590c4b72d165e252590fd7a
ibmjavanh01 <ibmjavanh01@iiht.tech>
2021-04-15 11:25:40 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
-@Component({
+@Component({ //metadata
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'MyApp';
+  title = 'MyApp'; //property
+
+  eat(){
+
+  }
 }
---
+++ src/app/userform/userform.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-userform',
+  templateUrl: './userform.component.html',
+  styleUrls: ['./userform.component.css']
+})
+export class UserformComponent implements OnInit {
+  title:string = 'Userform';
+  firstname='Ram';
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+}
