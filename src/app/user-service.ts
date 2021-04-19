52c570aee68bdb237192c275d379fd8689e1d0b8
ibmjavanh01 <ibmjavanh01@iiht.tech>
2021-04-19 10:12:20 +0530

    added pre-loading users

--- src/app/user-service.ts
+++ src/app/user-service.ts
@@ -1,16 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { User } from './User';
-
+const URL ='http://localhost:8080/user';
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
   constructor(private http: HttpClient) { }
+
   save(user: User) {
-    return this.http.post('http://localhost:8080/user', user, {
+    return this.http.post(URL, user, {
       headers: { "content-type": 'application/json' }
     });
+  }
+  getAllUsers(){
+    return this.http.get(URL);
   }
 }
--- src/app/userform/userform.component.ts
+++ src/app/userform/userform.component.ts
@@ -8,24 +8,29 @@ import { UserService } from '../user-service';
   styleUrls: ['./userform.component.css']
 })
 export class UserformComponent implements OnInit {  //controller
-  title:string = 'Userform';
-  user:User=new User(); //model -stores all form data
-  userArray:User[]=[];
-  constructor(private userService:UserService) { }
-  save(){
-    const promise = this.userService.save(this.user);
-    promise.subscribe(response=> {
+  title: string = 'Userform';
+  user: User = new User(); //model -stores all form data, Change detection works on mode
+  userArray: any;
+  constructor(private userService: UserService) { }
+  save() {
+    const observable = this.userService.save(this.user);
+    observable.subscribe(response => { //success function
       console.log(response);
       alert('user added..')
       this.userArray.push(Object.assign({}, this.user));
     },
-    error=> {
-      console.log(error);
-      alert('error happenned..')
-    })
+      error => {//error function
+        console.log(error);
+        alert('error happenned..')
+      })
     // this.user.firstname = 'John';
 
   }
   ngOnInit(): void {
+    const observable = this.userService.getAllUsers();
+    observable.subscribe(response => {
+      console.log(response);
+      this.userArray = response;
+     });
   }
 }
