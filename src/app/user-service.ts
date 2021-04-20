388a9e986f355e85a145902932899f8ec5b61bda
ibmjavanh01 <ibmjavanh01@iiht.tech>
2021-04-20 10:26:25 +0530

    added pipes

--- src/app/user-service.ts
+++ src/app/user-service.ts
@@ -1,14 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { User } from './User';
-const URL ='http://localhost:8080/user';
+const URL ='http://localhost:8080/user/';
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
   constructor(private http: HttpClient) { }
-
+  searchByName(name:string){
+    return this.http.get(URL+name);
+  }
   save(user: User) {
     return this.http.post(URL, user, {
       headers: { "content-type": 'application/json' }
@@ -18,6 +20,6 @@ export class UserService {
     return this.http.get(URL);
   }
   delete(userid:number  ){
-    return this.http.delete(URL+'/'+userid);
+    return this.http.delete(URL+userid);
   }
 }
