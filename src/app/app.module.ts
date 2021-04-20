72f2e9bcde0571bf0d37f6fac799317390981158
ibmjavanh01 <ibmjavanh01@iiht.tech>
2021-04-20 10:41:57 +0530

    added routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,23 @@ import { FormsModule } from "@angular/forms";
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { UserformComponent } from './userform/userform.component';
-
+import { RouterModule, Routes } from '@angular/router';
+import { SearchComponent } from './search/search.component';
+const appRoutes: Routes = [
+  { path: '', component: UserformComponent }, //default, Home page
+  { path: 'search', component: SearchComponent },
+];
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    UserformComponent
+    UserformComponent, SearchComponent
   ],
-  imports: [
-    BrowserModule, FormsModule,HttpClientModule,
+  imports: [RouterModule.forRoot(
+    appRoutes,
+    { enableTracing: true } // <-- debugging purposes only
+  ),
+    BrowserModule, FormsModule, HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent] //first component
